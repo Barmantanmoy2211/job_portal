a93e3ebd11e2ee17efa209b696529793dbdbb539
Barmantanmoy2211 <barmantanmoy59@gmail.com>
2024-08-28 21:48:46 +0530

    feat: Add constant file for user API endpoint
    chore: Add multer middleware for file upload
    
    feat: Create Home component
    
    chore: Add Toaster component for notifications
    
    chore: Update main.jsx to include Toaster component
    
    chore: Update server port to 8000
    
    chore: Update user registration route to include file upload
    
    chore: Add Label component
    
    chore: Update package.json with new dependencies
    
    chore: Update Navbar component with login and signup links
    
    chore: Add Input component
    
    chore: Add Sonner component for notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/shared/Navbar'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './components/auth/login'
+import Signup from './components/auth/signup'
+import Home from './components/Home'
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+  // {
+  //   path: "/jobs",
+  //   element: <Jobs />
+  // },
+  // {
+  //   path: "/description/:id",
+  //   element: <JobDescription />
+  // },
+  // {
+  //   path: "/browse",
+  //   element: <Browse />
+  // },
+  // {
+  //   path: "/profile",
+  //   element: <Profile />
+  // },
+  // // admin ke liye yha se start hoga
+  // {
+  //   path:"/admin/companies",
+  //   element: <ProtectedRoute><Companies/></ProtectedRoute>
+  // },
+  // {
+  //   path:"/admin/companies/create",
+  //   element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
+  // },
+  // {
+  //   path:"/admin/companies/:id",
+  //   element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
+  // },
+  // {
+  //   path:"/admin/jobs",
+  //   element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
+  // },
+  // {
+  //   path:"/admin/jobs/create",
+  //   element:<ProtectedRoute><PostJob/></ProtectedRoute> 
+  // },
+  // {
+  //   path:"/admin/jobs/:id/applicants",
+  //   element:<ProtectedRoute><Applicants/></ProtectedRoute> 
+  // },
+
+])
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={appRouter} />
     </div>
   )
 }
